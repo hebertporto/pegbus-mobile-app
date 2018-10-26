577239934d1cf06fe5e374eae56b111900600e60
Hebert Porto <hebertporto@gmail.com>
2018-10-25 22:08:32 -0500

    teste para mapear o retorno das rotas por ponto

--- src/mappers/__test__/stopInfo.spec.js
+++ src/mappers/__test__/stopInfo.spec.js
@@ -1,16 +1,21 @@
-import { scheduleStop } from './data'
-import { mapperScheduler } from '../busApiMapper'
+import { scheduleStop, routesOnStop } from './data'
+import { mapperScheduler, mapperBusRouterPerStop } from '../busApiMapper'
 
-describe('Stop Info Mapper', () => {
-  const data = mapperScheduler(scheduleStop);
+describe('Mappers', () => {
+  const data = mapperBusRouterPerStop(routesOnStop);
+
+  // test('extract info from api response', () => {
+  //   expect(data[0]).toEqual(
+  //     expect.arrayContaining([
+  //       expect.objectContaining({
+  //         type: 'END'
+  //       })
+  //     ])
+  //   )
+  // });
 
   test('extract info from api response', () => {
-    expect(data[0]).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          type: 'END'
-        })
-      ])
-    )
+    expect(data[0]).toEqual(1);
   });
+
 });
--- src/mappers/busApiMapper.js
+++ src/mappers/busApiMapper.js
@@ -1,6 +1,31 @@
 import { get } from 'lodash';
 
 export const mapperScheduler = (apiResponse) => {
-  const schedules = get('', []);
+  const schedules = get('stop-schedule.stop', []);
   return schedules;
 }
+
+export const mapperBusRouterPerStop = (apiResponse) => {
+  const fullInfo = get(apiResponse, 'routes', []);
+  const routes = fullInfo.map((route) => {
+    return route.number;
+  });
+  return routes;
+}
+
+export const stopInfo = {
+  id: '',
+  name: '',
+  stopNumber: '',
+  address: '',
+  buses: [],
+  schedules: [
+    {
+      number: '',
+      name: '',
+
+    }
+  ]
+}
+
+// 10628
