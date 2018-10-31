9077f2cb8df008c4489f2ae84a37462fc1fa5556
Hebert Porto <hebertporto@gmail.com>
2018-10-30 22:39:04 -0500

    styling

--- src/ui/ScheduleItem.js
+++ src/ui/ScheduleItem.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 import styles from './styles/scheduleItem.style';
 
 export default ScheduleItem = () => {
   return (
     <View style={styles.root}>
-      <View style={styles.number}></View>
-      <View style={styles.description}></View>
-      <View style={styles.hour}></View>
+      <View style={styles.number}>
+        <Text>A</Text>
+      </View>
+      <View style={styles.description}>
+        <Text>B</Text>
+      </View>
+      <View style={styles.hour}>
+        <Text>C</Text>
+      </View>
     </View>
   )
 }
