7eb4056e6c9f418739961a51083244daaf9db2e7
Hebert Porto <hebertporto@gmail.com>
2018-12-19 21:34:32 -0600

    remove tabs, upgrade react navigation

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,10 +1,26 @@
+// import React from 'react'
+// import { createSwitchNavigator } from 'react-navigation'
+
+// import MainTabNavigator from './MainTabNavigator'
+
+// export default createSwitchNavigator({
+//   // You could add another route here for authentication.
+//   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+//   Main: MainTabNavigator
+// })
+
 import React from 'react'
-import { createSwitchNavigator } from 'react-navigation'
+import { createStackNavigator, createAppContainer } from 'react-navigation'
 
-import MainTabNavigator from './MainTabNavigator'
+import { HomeScreen } from './../screens/home/HomeScreen'
 
-export default createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator
-})
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen
+  },
+  {
+    initialRouteName: 'Home'
+  }
+)
+
+export default createAppContainer(AppNavigator)
